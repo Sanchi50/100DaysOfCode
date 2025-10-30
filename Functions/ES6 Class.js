f037332819845ead4a9396884f5561c148c8ed15
Sanchi Wahane <wahane.sanchi02@gmail.com>
2025-10-30 18:54:33 +0530

    ES5 Class

--- Functions/ES6 Class.js
+++ Functions/ES6 Class.js
@@ -58,50 +58,68 @@ Hello, my name is John and I am 20 years old. I am also a student studying CS.
 
 I am studying CS and my GPA is 9.*/
 
-function main (){
+function main() {
 
-
-class Person{
-    constructor (name,age,gender){
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
+  class Person {
+    constructor(name, age, gender) {
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
     }
 
-    speak(){
-        console.log(`Hello my name is ${this.name} and I am ${this.age} years old.`);
+    speak() {
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
+  }
+
+  class Student {
+    constructor(person, major, gpa) {
+      this.person = person;  // storing Person object
+      this.major = major;
+      this.gpa = gpa;
+    }
+
+    study() {
+      console.log(`I am studying ${this.major} and my GPA is ${this.gpa}.`);
+    }
+
+    speak() {
+      console.log(
+        `Hello, my name is ${this.person.name} and I am ${this.person.age} years old. I am also a student studying ${this.major}.`
+      );
+    }
+  }
+
+  const person1 = new Person("John", 20, "M");
+  person1.speak();
+
+  const stud1 = new Student(person1, "CS", 9);
+  stud1.speak();
+  stud1.study();
+
+  return { Person, Student };
 }
 
-class Student{
-    constructor (person, major, gpa){
-    this.person = person;
-    this.major = major;
-    this.gpa = gpa;
-    }
-    
-    
-    study(){
-        console.log(`I am studying ${this.major} and my GPA is ${this.gpa}. `);
-    }
-    
-    speak(){
-        console.log(`Hello, my name is ${this.person.name} and I am ${this.person.age} years old. I am also a student studying ${this.major}`);
-    }
-}
-
-const person1 = new Person("John", 20, "M");
-person1.speak();
-
-const stud1 = new Student(person1,"CS",9);
-stud1.speak();
-stud1.study();
-
-return {Person,Student};
-}
 main();
+    
 
 
+//What will be the output:
+class Student {
+    name;
+    sec;
+    #rollno;
+    constructor(name, sec, rollno){
+        this.name = name;
+        this.sec = sec;
+        this.rollno = rollno;
+    }
+}
 
+const ram = new Student("Ram", "B", 34356);
+console.log(ram);
 
-
+//Student {name: "Ram", sec: "B" , rollno: 34356 #rollno: undefined}
+//It is mandatory to use #while working with the private properties,however it will not
+//give an error if we are not using #, instead it will assume `rollno` as a new property
+//and assign a value to it.
