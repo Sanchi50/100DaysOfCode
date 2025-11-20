157144abb586afb86aae11e9795d8673840c3cb7
Sanchi Wahane <wahane.sanchi02@gmail.com>
2025-11-20 12:39:40 +0530

    break & continue

--- Arrays/first5even.js
+++ Arrays/first5even.js
@@ -1,2 +1,16 @@
 //get first 5 even numbers from an array
 const inputs = [11,32,12,45,65,94,7,78,10,100,131,67,81,92,53];
+const evenNumbers = [];
+for (let i of inputs){
+    if(i % 2 !== 0){
+        continue;
+
+    }
+    evenNumbers.push(i);
+    if(evenNumbers.length === 5){
+        break;
+    }
+    console.log(evenNumbers);
+    //yaha sirf 4 print honge qk break ke baad console kar rahehai to last vala print nahi hoga.
+}
+console.log(evenNumbers);
