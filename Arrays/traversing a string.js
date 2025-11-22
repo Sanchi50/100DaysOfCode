1ee18200015556e97aa9c047dce3d6b471d31d8e
Sanchi Wahane <wahane.sanchi02@gmail.com>
2025-11-22 21:43:45 +0530

    Traversing a string

---
+++ Arrays/traversing a string.js
@@ -0,0 +1,14 @@
+let str = "Javacsript is an amazing language";
+let charToFind = "a";
+
+function countChars(str, ch){
+    let count = 0;
+    for (let i=0; i<str.length;i++){
+        if(str.charAt(i) === ch){
+            count++;
+        }
+    }
+    console.log(`Occurance of character ${ch} is ${count} times.`)
+}
+
+countChars(str, charToFind);
