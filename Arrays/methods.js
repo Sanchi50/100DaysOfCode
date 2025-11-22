b9b66083fbd26ee17a12a68d21765bf4b4f1ae12
Sanchi Wahane <wahane.sanchi02@gmail.com>
2025-11-22 21:13:29 +0530

    sum Of arrays

--- Arrays/methods.js
+++ Arrays/methods.js
@@ -72,3 +72,44 @@ console.log(arr6);//false (starts checking from index no. 1)
 
 [5, 6, 7, 8, 9].slice(3, 10)
 //8,9 (array khatam ho chuka hai, mai end tak de dunga)
+
+//splice()
+const arr1 = [10, 20, 30, 40];
+arr.splice(1, 2);
+//[10,40]= arr
+//return [20,30]
+
+const arr11 = ["a", "b", "c", "d"];
+arr.splice(2, 0, "X");
+//["a" , "b", "X" ,"c", "d"]
+
+const arr111 = [5, 6, 7, 8];
+arr.splice(1, 1, 99, 100);
+//new array = [5,99,100,7,8]
+//return [6]
+
+const ar1 = [1, 2, 3, 4, 5];
+ar1.splice(3, 10);
+//(value 10) matlab jitna mile sab nikaal do 3rd index se
+//new = [1,2,3]
+//return = [4,5]
+
+const ar2 = [100, 200, 300];
+ar2.splice(0, 0, 999);
+//new = [999,100,200,300]
+
+
+const ar3 = ["x", "y", "z"];
+ar3.splice(1, 1, "A", "B");
+//["x", "A", "B", "z"]
+
+
+const ar4 = [9, 8, 7];
+ar4.splice(-1, 1);
+//[9,7]
+//return[8]
+
+const ar5 = [1, 2, 3, 4];
+ar5.splice(2, 2, "X");
+//[1,2,"X"]
+//[3,4]
--- Arrays/sumOfArray.js
+++ Arrays/sumOfArray.js
@@ -12,3 +12,17 @@ Omit those elements which are divisible by 5
 Sum all the elements which satisfy the given conditions
 Return the sum*/
 let arr = [1,6,8,3,5,20,10];
+
+function sumOfArray(arr){
+    let sum = 0;
+    for(let i=0; i< arr.length; i++){
+        const num = arr[i];
+        if(num % 5 === 0){
+            continue;
+        }
+        if(num % 2 ===0){
+            sum = sum + num;
+        }
+    }
+    return sum;
+}
