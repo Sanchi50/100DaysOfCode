98d27d65a1273e45a805f1cb189c1893a5cd3161
Sanchi Wahane <wahane.sanchi02@gmail.com>
2026-01-10 01:24:51 +0530

    refactoring classes

---
+++ Classes/refactoring.js
@@ -0,0 +1,51 @@
+/**or this question, you need to refactor the Car Class and its methods into the constructor function.
+
+
+
+Note:
+Make sure to keep the methods that were initially prototypal as prototype methods and retain the methods that were originally part of the class as regular methods within the constructor function. */
+
+
+function main() {
+  function Car(make, model, year, color, mileage) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.mileage = mileage;
+
+    
+    this.getMake = function() {
+      return this.make;
+    };
+  }
+
+  
+  Car.prototype.getModel = function() {
+    return this.model;
+  };
+
+  Car.prototype.getYear = function() {
+    return this.year;
+  };
+
+  Car.prototype.getColor = function() {
+    return this.color;
+  };
+
+  Car.prototype.getMileage = function() {
+    return this.mileage;
+  };
+
+  const myCar = new Car("Toyota", "Camry", 2020, "blue", 5000);
+  console.log(myCar.getMake());
+  console.log(myCar.getModel());
+  console.log(myCar.getYear());
+  console.log(myCar.getColor());
+  console.log(myCar.getMileage());
+
+  // Do not modify return
+  return Car;
+}
+
+main();
